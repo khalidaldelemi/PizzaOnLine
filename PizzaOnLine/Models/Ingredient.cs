32741f768c90e5537d8194e523ba63a9013a65a8
Khalid <Khalid@DESKTOP-8LLHGDQ>
2017-08-29 13:54:32 +0200

    Test

---
+++ PizzaOnLine/Models/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PizzaOnLine.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public List<DishIngredient> DishIngredients { get; set; }
+    }
+}
